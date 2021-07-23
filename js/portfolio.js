16b337fa0f3577675d30a438b75c22a9281de49b
Kelli Parker <iamkelliparker@gmail.com>
2021-07-22 20:07:25 -0400

    small css updates and restoring right click

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,13 +1,3 @@
-if (document.addEventListener) {
-    document.addEventListener('contextmenu', function(e) {
-        e.preventDefault();
-    }, false);
-} else {
-    document.attachEvent('oncontextmenu', function() {
-        window.event.returnValue = false;
-    });
-}
-
 $(function() {
 	var galleries = {
 		
